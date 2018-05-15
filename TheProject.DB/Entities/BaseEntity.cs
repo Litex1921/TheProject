9238438b45010cf39be2c402ea36a5bd89965658
Litex1921 <38464079+Litex1921@users.noreply.github.com>
2018-05-15 14:04:30 +0300

    TheProject

--- TheProject.DB/Entities/BaseEntity.cs
+++ TheProject.DB/Entities/BaseEntity.cs
@@ -1,18 +1,18 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.ComponentModel.DataAnnotations;
+﻿using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
-
 
 namespace TheProject.DB.Entities
 {
-    public class BaseEntity
+    class BaseEntity : IBaseEntity<int, System.DateTime>
     {
-        [DatabaseGeneratedAttribute(DatabaseGeneratedOption.Identity)]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
         [Key]
         public int Id { get; set; }
+
+        [DatabaseGenerated(DatabaseGeneratedOption.Computed)]
+        public System.DateTime CreatedTime { get; set; }
+
+        [DatabaseGenerated(DatabaseGeneratedOption.Computed)]
+        public System.DateTime UpdatedTime { get; set; }
     }
 }
---
+++ TheProject.DB/Entities/IBaseEntity.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TheProject.DB.Entities
+{
+    class IBaseEntity <T,K>
+    {
+        T Id { get; set; }
+
+        K CreatedTime { get; set; }
+
+        K UpdatedTime { get; set; }
+    }
+}
